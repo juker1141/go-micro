eab19c62e02b34c248ac66e890f560107d9e57da
Ryu <juker1141@gmail.com>
2023-07-26 08:47:56 +0800

    update logger-service with RPC

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -4,17 +4,20 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
+
+	"log-service/data"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
-	webPort = "80"
-	rpcPort = "5001"
+	webPort  = "80"
+	rpcPort  = "5001"
 	mongoURL = "mongodb://mongo:27017"
 	gRpcPort = "50001"
 )
@@ -35,7 +38,7 @@ func main() {
 	client = mongoClient
 
 	// create a context in order to disconnect mongo
-	ctx, cancel := context.WithTimeout(context.Background(), 15 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	// close connection
@@ -43,11 +46,15 @@ func main() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
 		}
-	} ()
+	}()
 
 	app := Config{
 		Models: data.New(client),
 	}
+
+	// Register the RPC Server
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
 
 	// start web server
 	app.serve()
@@ -55,13 +62,30 @@ func main() {
 
 func (app *Config) serve() {
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%s", webPort),
+		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
 	}
 }
 
